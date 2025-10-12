000409e87c9dbd2b220a93f466431d0320eec990
Mustapha Aliane <allouchi@hotmail.fr>
2025-10-12 16:09:31 +0200

    add alert

--- src/app/services/alert/alertService.ts
+++ src/app/services/alert/alertService.ts
@@ -11,31 +11,46 @@ export interface ToastData {
 
 @Injectable({ providedIn: 'root' })
 export class AlertService {
-  delete = ' été supprimée avec succès !';
-  add = ' été ajoutée avec succès !';
-  update = ' été mise à jour avec succès !';
-  logout = 'A bientôt !'
+  deleteMessage = ' été supprimée avec succès !';
+  addMessage = ' été ajoutée avec succès !';
+  updateMessage = ' été mise à jour avec succès !';
+  logoutMessage = 'A bientôt !';
   private alertSubject = new Subject<ToastData>();
   toast$ = this.alertSubject.asObservable();
 
-  show(action: string, composant: string, message: string, title?: string, type: ToastData['type'] = 'success', delay = 3000) {
+  show(
+    action: string,
+    composant: string,
+    message: string,
+    title?: string,
+    type: ToastData['type'] = 'success',
+    delay = 7000
+  ) {
+    composant =
+      'Le composant ' +
+      composant.charAt(0).toUpperCase() +
+      composant.slice(1).toLowerCase();
+
     switch (action) {
       case 'DELETE': {
+        message = composant + this.deleteMessage;
         break;
       }
 
       case 'ADD': {
+        message = composant + this.addMessage;
         break;
       }
 
       case 'UPDATE': {
+        message = composant + this.updateMessage;
         break;
       }
 
       case 'LOGOUT': {
+        message = composant + this.logoutMessage;
         break;
       }
-
     }
 
     this.alertSubject.next({ message, title, type, delay });
--- src/app/services/shared/app-init-service.ts
+++ src/app/services/shared/app-init-service.ts
@@ -2,7 +2,7 @@
 import { Injectable } from '@angular/core';
 import { CompanyService } from '../companies/company-service';
 import Company from '../../models/Company';
-import { AlertService } from '../alert/alert-messages.service';
+import { AlertService } from '../alert/alertService';
 
 @Injectable({
   providedIn: 'root',
@@ -26,7 +26,7 @@ export class AppInitService {
     });
   }
 
-  private onSuccess(companies: Company[]) {   
+  private onSuccess(companies: Company[]) {
     this.selectedCompany = companies.find(
       (company) => company.checked === true
     )!;
@@ -36,9 +36,9 @@ export class AppInitService {
     const message: string = error.message;
 
     if (message.includes('Http failure')) {
-      this.alertService.show('Problème serveur', 'error');
+      this.alertService.show('SERVER_ERROR', 'Problème serveur', 'error');
     } else {
-      this.alertService.show(message, 'error');
+      this.alertService.show('', message, 'error');
     }
   }
 
